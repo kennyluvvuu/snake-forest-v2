8007fb7b5e6d919e59b07618a7fecaed12f8a53c
Sergey Sokolovsky <sergey270307@gmail.com>
2025-12-12 01:47:38 +0700

    refactored req and res schemas

--- backend/src/schemas/animal.schema.ts
+++ backend/src/schemas/animal.schema.ts
@@ -1,53 +1,43 @@
 import { z } from "zod"
-import { ImageSchema } from "./image.schema"
 
 export const AnimalSchema = z.object({
+    id: z.string(),
     commonName: z.string().min(2, "Common name is required"),
     morph: z.string().min(2),
     sex: z.enum(["male", "female", "unknown"]),
     price: z.number(),
-})
-
-export const AnimalSchemaWithId = AnimalSchema.extend({
-    id: z.string()
-})
-
-export const AnimalSchemaFull = AnimalSchemaWithId.extend({
     description: z.string().min(25, {
         message: "Description must be > 25 symbols"
     }).max(500, {
         message: "Description must be < 500 symbols"
-    })
+    }),
+    imagesUrl: z.array(z.string()).max(10, {
+        message: "Too many images(cant be more 10)"
+    }),
 })
 
-export const CreateAnimalSchema = AnimalSchema.extend({
-    description: z.string().min(25, {
-        message: "Description must be > 25 symbols"
-    }).max(500, {
-        message: "Description must be < 500 symbols"
-    })
+export const CreateAnimalSchema = AnimalSchema.omit({
+    id: true,
+    imagesUrl: true
 })
 
 export type CreateAnimalReq = z.infer<typeof CreateAnimalSchema>
-export type CreateAnimalRes = z.infer<typeof AnimalSchemaFull>
-export type UpdateAnimalReq = z.infer<typeof AnimalSchemaWithId>
-export type UpdateAnimalRes = z.infer<typeof AnimalSchemaWithId>
+export type CreateAnimalRes = z.infer<typeof AnimalSchema>
 
-export const GetAnimalPreviewSchema = AnimalSchemaWithId.extend({
+export const UpdateAnimalSchema = AnimalSchema.omit({
+    id: true,
+    imagesUrl: true
+})
+
+export type UpdateAnimalReq = z.infer<typeof UpdateAnimalSchema>
+export type UpdateAnimalRes = z.infer<typeof AnimalSchema>
+
+export const GetAnimalPreviewSchema = AnimalSchema.extend({
     preview: z.array(z.string()),
+}).omit({
+    description: true,
+    imagesUrl: true
 })
 
 export type GetAnimalPreviewRes = z.infer<typeof GetAnimalPreviewSchema>
-
-export const GetAnimalSchema = AnimalSchemaWithId.extend({
-    images: z.array(z.string()).max(10, {
-        message: "Too many images(cant be more 10)"
-    }),
-    description: z.string().min(25, {
-        message: "Description must be > 25 symbols"
-    }).max(500, {
-        message: "Description must be < 500 symbols"
-    })
-})
-
-export type GetAnimalRes = z.infer<typeof GetAnimalSchema>
+export type GetAnimalRes = z.infer<typeof AnimalSchema>
