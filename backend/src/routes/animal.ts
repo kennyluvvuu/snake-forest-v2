169b69d95d659a576262e93ce3b661e86171d213
Sergey Sokolovsky <sergey270307@gmail.com>
2025-12-13 01:44:37 +0700

    moved to fastify from express, removed middlewares

--- backend/src/routes/animal.ts
+++
@@ -1,9 +0,0 @@
-import { type Request, type Response } from "express";
-import express from "express";
-import { validator } from "../middlewares/validator";
-import { type IAnimalController } from "../interfaces/animal.controller.interface";
-import * as schemas from "../schemas/animal.schema";
-
-const api = express.Router();
-
-api.get("/", (req: Request, res: Response) => {});
